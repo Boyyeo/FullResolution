ba51b8c8dc55cd50e401e8e1431b7d480b005e5b
Boyyeo <87563842+Boyyeo@users.noreply.github.com>
2023-12-24 01:32:43 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-CUDA_VISIBLE_DEVICES=0 python train.py --bs 128 --lr 0.0005 --out_dir 'output/' --name 'fullres-compress' --data_path 'CIFAR10-images/train' --save_record False --iterations 16
-CUDA_VISIBLE_DEVICES=1 python mnist_autoencoder.py
+### For one-shot lstm
+CUDA_VISIBLE_DEVICES=0 python train.py --bs 32 --lr 0.0005 --out_dir 'output/' --name 'lstm-oneshot' --data_path 'CIFAR-10-images/train' --save_record True --iterations 16 --recon_fw 'one-shot' --arch 'lstm'
 
+### For additive lstm
+CUDA_VISIBLE_DEVICES=0 python train.py --bs 32 --lr 0.0005 --out_dir 'output/' --name 'lstm-additive' --data_path 'CIFAR-10-images/train' --save_record True --iterations 16 --recon_fw 'one-shot' --arch 'lstm'
