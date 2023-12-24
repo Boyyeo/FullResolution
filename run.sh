ad614c9df30c699f3018f2692bb73dbaf647146c
Boyyeo <87563842+Boyyeo@users.noreply.github.com>
2023-12-24 22:54:29 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ CUDA_VISIBLE_DEVICES=0 python train.py --bs 32 --lr 0.0005 --out_dir 'output/' -
 CUDA_VISIBLE_DEVICES=0 python train.py --bs 32 --lr 0.0005 --out_dir 'output/' --name 'lstm-resscale' --data_path 'CIFAR-10-images/train' --save_record True --iterations 16 --recon_fw 'residual-scaling' --arch 'lstm'
 
 ### For Evaluation 
+get_kodak.sh
 CUDA_VISIBLE_DEVICES=0 python train.py  --recon_fw 'one-shot' --arch 'lstm' --epoch 0 --save_record True --resume_ckpt output/lstm-additive-20231224-045144/checkpoint/model_0.pyt
-CUDA_VISIBLE_DEVICES=0 python train.py  --recon_fw 'residual-scaling' --arch 'lstm' --epoch 0 --save_record True 
 
 
