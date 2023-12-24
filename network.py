aab01b5917fd6defb0daa35ce5bab99fae2a4072
謝惠喻 <hueiyu.mg08@nycu.edu.tw>
2023-12-24 14:04:39 +0800

    Add files via upload

--- network.py
+++ network.py
@@ -125,3 +125,36 @@ class DecoderCell(nn.Module):
 
         x = F.tanh(self.conv2(x)) / 2
         return x, hidden1, hidden2, hidden3, hidden4
+
+
+class GainEstimatorCell(nn.Module):
+    def __init__(self):
+        super(GainEstimatorCell, self).__init__()
+
+        # [32, 3, 32, 32] floor((w+2*pad-ks)/s)+1
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=2, padding=1) # [32, 32, 16, 16]
+
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1) # [32, 32, 8, 8]
+
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1) # [32, 32, 4, 4]
+
+        self.conv4 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1) # [32, 32, 2, 2]
+
+        self.conv5 = nn.Conv2d(in_channels=32, out_channels=1, kernel_size=2, stride=2, padding=1) # [32, 1, 2, 2]
+
+    def forward(self, x):
+        y1 = self.conv1(x)
+        y1_ = F.elu(y1)
+        y2 = self.conv2(y1_)
+        y2_ = F.elu(y2)
+        y3 = self.conv3(y2_)
+        y3_ = F.elu(y3)
+        y4 = self.conv4(y3_)
+        y4_ = F.elu(y4)
+        y5 = self.conv5(y4_)
+        y5_ = F.elu(y5)
+
+        # Add a constant of 2 to the output
+        output = y5_ + 2.0
+
+        return output
